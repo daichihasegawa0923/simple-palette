2ccb4b139c1fffc354f0576372363a9dd6e8ca74
daichihasegawa0923 <punyachi@yahoo.co.jp>
2021-11-23 00:50:07 +0900

    initial commit

---
+++ simple-table/simple-table-component/src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const SimpleTable = () =>
+{
+    return (
+        <>
+            <table>
+                <th>
+
+                </th>
+                <td>
+                    <tr>a</tr>
+                </td>
+            </table>
+        </>
+    );
+}
+
+export default SimpleTable;
