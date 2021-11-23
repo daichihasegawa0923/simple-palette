5a16e48352ce7b99b1dac0bf4b0ddf3fb802477b
daichihasegawa0923 <daichihasegawa052@gmail.com>
2021-11-23 12:40:44 +0900

    add something

--- simple-table/src/App.js
+++ simple-table/src/App.js
@@ -1,27 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 import {SimpleTable} from './components/simple-table-component';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <SimpleTable 
-      data = {[{name: 'aaa', age: 12},{name: 'aaa', age: 12}]} 
-      accessors = {[{propName: 'name', displayName: '名前'},{propName: 'age', displayName: '年齢'}]} />
+      data = {[
+        {name: 'aaa', age: 26},
+        {name: 'bbb', age: 22},
+        {name: 'bbb', age: 35},
+        {name: 'eee', age: 122},
+        {name: 'fff', age: 92},]} 
+      accessors = {[{propName: 'name', displayName: '名前'},{propName: 'age', displayName: '年齢', isInputType: true}]} />
       <SimpleTable data={[]} accessors={[]}/>
       <SimpleTable data={[]} accessors={[]}><div>arisona</div></SimpleTable>
     </div>
--- simple-table/src/components/simple-table-component.tsx
+++ simple-table/src/components/simple-table-component.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components'
 
-export interface Accessor{
+export interface ColumnDefinition{
     propName: string;
     displayName: string;
+    isInputType?: boolean;
 }
 
 export interface SimpleTableProps{
-    accessors: Accessor[];
+    accessors: ColumnDefinition[];
     data: any[];
     children?: React.ReactNode;
 }
 
+export interface SimpleTableStyleProps {
+    borderWidth?: number;
+}
+
 export const SimpleTable = ({accessors,data,children}: SimpleTableProps) => {
+    const [inputData, setInputData] = useState(data);
+    const sort = (propName: string, isAscending: boolean) => {
+        inputData.sort((d1,d2)=>{
+            const judge = isAscending ? d1[propName] < d2[propName] : d1[propName] > d2[propName];
+            if(judge){
+                return -1;
+            } 
+            return 1
+        });
+        setInputData([...inputData]);
+    }
 
     if(accessors.length === 0 || data.length === 0 ){
         if (children) return (children);
@@ -22,11 +39,23 @@ export const SimpleTable = ({accessors,data,children}: SimpleTableProps) => {
     <>
     <table>
         <thead>
-            {accessors.map(accsessor => (<td>{accsessor.displayName}</td>))}
+            {accessors.map(accsessor => (<td>
+                {accsessor.displayName}
+                <button style = {{border: 'none', backgroundColor: 'transparent'}} onClick={()=>{sort(accsessor.propName, true)}}>
+                    ↓
+                </button>
+                <button style = {{border: 'none', backgroundColor: 'transparent'}} onClick={()=>{sort(accsessor.propName, false)}}>
+                    ↑
+                </button>
+            </td>))}
         </thead>
-        {data.map(d =>(
+        {inputData.map(d =>(
         <tr>
-            {accessors.map(accessor => (<td>git{d[accessor.propName]}</td>))}
+            {
+            accessors.map(accessor => {
+                return (<td>{accessor.isInputType ?(<input value={d[accessor.propName]}/>) : d[accessor.propName]}</td>);
+            } )
+            }
         </tr>))}
     </table>
     </>);
